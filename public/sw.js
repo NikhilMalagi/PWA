1f2ca3313dae9df4510523180c37aed275085404
NikhilMalagi <nikhilmalagi111@gmail.com>
2021-03-27 16:56:52 +0530

    SW caching

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,35 @@
 
 self.addEventListener('install', function(event) {
-  /* Executes before SW is registered */
   console.log('[Service Worker] Installing Service Worker ...', event);
+
+  /* Need to use waitUntil to make it synchronous to avoid trying to fetch from cache before its setup */
+  event.waitUntil(
+      /* opens a cahce and return a promise */
+      caches.open('static').then((cache) => {
+        console.log('[Service Worker] Precaching app shell');
+        /* add : fires req,gets response and stores it */
+        cache.add('/index.html')
+        cache.add('/src/js/app.js')
+        /* THINK IN TERMS OF REQ!!! - as thse are the keys that will be stored in the cache */
+        cache.add('/')
+      })
+  )
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
-  /* Should work without this : But ensures that the SW is registerd corretly */
+  console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
-  /* Fetch can be used as proxy for calls
-     And can be used for both request and response
-  */
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+      /* will match the key here(req object) */
+      caches.match(event.request)
+      .then((response) => {
+        if(response){return response;}
+        else{ return fetch(event.request)}
+      })
+      /* Always use only then because : if not present will recieve null */
+      
+  );
 });
